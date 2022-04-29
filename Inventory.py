552923e4a95617298f44704a9475b4dee07700c4
Cooper Riley <89549905+cooper350@users.noreply.github.com>
2022-04-29 16:58:20 -0500

    Add files via upload

---
+++ Inventory.py
@@ -0,0 +1,8 @@
+class Inventory:
+    import item
+            
+    def ViewInventory():
+        print("Inventory: " + self.item)
+        
+    def ViewInventoryByCategory( Category ):
+        print("Inventory: " + self.Category)
