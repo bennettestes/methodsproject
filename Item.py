6ed93a7b246b96a2163d79b80cdebd68178f3efb
Wisp19 <90427912+Wisp19@users.noreply.github.com>
2022-05-02 14:59:49 -0500

    Add files via upload

---
+++ Item.py
@@ -0,0 +1,33 @@
+
+
+class Item:
+
+    def __init__(self, Name, Price, ItemID, Stock, Category):
+
+    self.Name = Name
+    self.Price = Price
+    self.ItemID = ItemID
+    self.Stock = Stock
+    self.Category = Category
+
+    
+    def GetName(self):
+    
+    return self.Name 
+    
+    def GetPrice(self):
+    
+    return self.Price
+    
+    def GetItemID(self):
+    
+    return self.ItemID
+    
+    def GetStock(self):
+    
+    return self.Stock
+    
+    def GetCategory(self):
+    
+    return self.Category
+    
