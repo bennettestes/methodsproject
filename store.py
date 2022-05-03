6ae3244be90d775b6595966aab7c7e6e534aeeb4
bennettestes <104655072+bennettestes@users.noreply.github.com>
2022-05-02 23:34:54 -0500

    Add files via upload

--- store.py
+++ store.py
@@ -19,31 +19,40 @@ class store:
         newcustomer.set_CardNumber(CardNumber)
         return newcustomer
 
+    def AddOrderHistory(self, orderHistory):
+        self.OrderHistory = []
+        self.OrderHistory.append(orderHistory)
+
+    
+    def ViewOrderHistory(self):
+        print("Showing Order History...\n")
+        for x in range(len(self.OrderHistory)):
+            print(self.OrderHistory[x])    
 
 
     def Login():
         username = input("Enter username: ")
 
         with open("customer.txt", 'r') as f:
-            lines = f.readlines()
+            
             current_user = ""
-            while current_user == "":
-                for line in lines:
-                    currentline = line.split(",")
+            
+            while True:
+                try:
+                    lines = f.readline()
+                    currentline = lines.split(",")
                     if currentline[2] == username:
                         password = input("Enter Password: ")
                         if currentline[3] == password:
                             current_user = store()
                             current_user.set_CurrentUser(username)
                             return current_user
-                        else:
-                            print("Incorrect password for", username)
-                            return ""
-                    else:
-                        print("Error: Username not found.")
-                        return ""
-        f.close()
-            
+                
+                        else: 
+                            print("Incorrect Username or Password. ")
+                except IndexError:
+                    exit()
+        
 
 
     def Logout():
